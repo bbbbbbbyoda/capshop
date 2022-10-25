b7910b5505e2dedc35e11a93c15822c1276a9a63
bbbbbbbyoda <tilekmam@gmail.com>
2022-10-25 22:36:36 +0600

    ~

--- cap/settings.py
+++ cap/settings.py
@@ -131,7 +131,8 @@ AUTH_USER_MODEL = 'account.MyUser'
 MEDIA_URL = 'mediafiles/'
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '')
-STATIC_ROOT = '/var/www/example.com/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
